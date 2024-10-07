77d00113275daf277af1fb45b3db83647147155d
yvictor <yvictor3141@gmail.com>
2024-10-07 18:37:20 +0800

    feat: apply api key and ca

--- src/sj_trading/__init__.py
+++ src/sj_trading/__init__.py
@@ -1,9 +1,33 @@
+import os
+
 import shioaji as sj
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def hello() -> None:
     print("Hello from sj-trading!")
 
+
 def get_shioaji_client() -> sj.Shioaji:
-    api =  sj.Shioaji()
+    api = sj.Shioaji()
     print("Shioaji API created")
     return api
+
+
+def main():
+    api = sj.Shioaji(simulation=True)
+    api.login(
+        api_key=os.environ["API_KEY"],
+        secret_key=os.environ["SECRET_KEY"],
+        fetch_contract=False
+    )
+    api.activate_ca(
+        ca_path=os.environ["CA_CERT_PATH"],
+        ca_passwd=os.environ["CA_PASSWORD"],
+    )
+
+
+if __name__ == "__main__":
+    main()
